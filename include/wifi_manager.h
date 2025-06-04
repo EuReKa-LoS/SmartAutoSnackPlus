0fd04353b35dcadc76610f58ad29f4a050aba960
EuReKa-LoS <adn.lan.90}gmail.com>
2025-06-04 21:51:03 +0200

    ✨✨
    Prepare for AD HOC

--- include/wifi_manager.h
+++ include/wifi_manager.h
@@ -2,9 +2,24 @@
 #define WIFI_MANAGER_H
 
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include "status.h"
 
 void initWiFi();
 void updateWiFiStatus(SystemStatus &status);
+
+class WiFiManagerCustom {
+public:
+    void begin();                  // Lance le Wi-Fi en mode config ou STA
+    void setDebugMode(bool debug); // Force le mode config pour debug
+
+private:
+    bool debugMode = false;
+    void startAccessPoint();      // Mode config via AP
+    void connectToWiFi();         // Mode station
+};
+
+extern WiFiManagerCustom wifi_manager;
+
 
 #endif
